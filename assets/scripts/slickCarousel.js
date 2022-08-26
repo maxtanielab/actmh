167c0dc41f5253bed2eb8c0dd55f1218ab283b69
Maxtanie Petit Dol <108846141+maxtanielab@users.noreply.github.com>
2022-08-26 23:15:23 +0200

    Add files via upload

--- assets/scripts/slickCarousel.js
+++ assets/scripts/slickCarousel.js
@@ -1,29 +1,22 @@
 export const slider = $(".slick-slider").slick({
 	infinite: false,
+	// autoplay: true,
+	autoplaySpeed: 3000,
 	slidesToShow: 3,
 	slidesToScroll: 1,
-	autoplay: true,
-	autoplaySpeed: 3000,
 	responsive: [
 		{
-			breakpoint: 1100,
+			breakpoint: 1024,
 			settings: {
 				slidesToShow: 2,
-				slidesToScroll: 2
+				slidesToScroll: 1
 			}
 		},
 		{
-			breakpoint: 768,
+			breakpoint: 480,
 			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
-			}
-		},
-		{
-			breakpoint: 500,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
+				slidesToShow: 1,
+				slidesToScroll: 1
 			}
 		}
 	],
